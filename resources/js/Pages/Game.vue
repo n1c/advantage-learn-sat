31d5b78d3400eee390526f0cde116bc6aaf1f534
Chris Muller <chris@Chriss-MacBook-Pro.local>
2022-04-04 17:16:47 +0200

    Add additional stylings to see text in dark mode

--- resources/js/Pages/Game.vue
+++ resources/js/Pages/Game.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { Head, Link } from '@inertiajs/inertia-vue3';
-import { Inertia } from '@inertiajs/inertia';
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import { Inertia } from "@inertiajs/inertia";
 
 const props = defineProps({
   description: {
@@ -27,36 +27,27 @@ const form = {
 };
 
 function submit() {
-  Inertia.get(route('game'), form);
+  Inertia.get(route("game"), form);
 }
 </script>
 
 <template>
   <Head title="Game!" />
 
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center">
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center"
+  >
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 dark:text-white">
       <h1 class="text-xl">
         {{ description }}
       </h1>
 
-      <Link
-        v-show="isFinished"
-        :href="route('game')"
-        class="underline"
-      >
+      <Link v-show="isFinished" :href="route('game')" class="underline">
         Reset
       </Link>
 
-      <form
-        v-show="!isFinished"
-        class="pt-4"
-        @submit.prevent="submit"
-      >
-        <label
-          for="left"
-          class="block py-2"
-        >
+      <form v-show="!isFinished" class="pt-4" @submit.prevent="submit">
+        <label for="left" class="block py-2">
           Left:
           <input
             id="left"
@@ -64,19 +55,12 @@ function submit() {
             type="number"
             step="1"
             required
-          >
-          <button
-            class="text-xl p-3"
-            @click="form.left++"
-          >
-            +
-          </button>
+            class="text-gray-900"
+          />
+          <button class="text-xl p-3" @click="form.left++">+</button>
         </label>
 
-        <label
-          for="right"
-          class="block py-2"
-        >
+        <label for="right" class="block py-2">
           Right:
           <input
             id="right"
@@ -84,28 +68,14 @@ function submit() {
             type="number"
             step="1"
             required
-          >
-          <button
-            class="text-xl p-3"
-            @click="form.right++"
-          >
-            +
-          </button>
+            class="text-gray-900"
+          />
+          <button class="text-xl p-3" @click="form.right++">+</button>
         </label>
 
-        <button
-          type="submit"
-          class="bg-blue-500 p-2"
-        >
-          Submit
-        </button>
+        <button type="submit" class="bg-blue-500 p-2">Submit</button>
 
-        <button
-          class="pl-2"
-          type="reset"
-        >
-          Reset
-        </button>
+        <button class="pl-2" type="reset">Reset</button>
       </form>
     </div>
   </div>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,18 +1,17 @@
 <script setup>
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import { Head, Link } from "@inertiajs/inertia-vue3";
 </script>
 
 <template>
   <Head title="Welcome" />
 
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center">
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center"
+  >
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
       <ul>
         <li>
-          <Link
-            :href="route('game')"
-            class="underline"
-          >
+          <Link :href="route('game')" class="underline dark:text-white">
             Simple Game
           </Link>
         </li>
